3e7fa1813c2c5be3661b38f215c2977eae7a420a
tien <thetien9a555@gmail.com>
2025-07-18 14:20:53 +0700

    finish GTA VI page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+import FirstVideo from "./sections/FirstVideo";
+import Jason from "./sections/Jason";
+import SecondVideo from "./sections/SecondVideo";
+import Lucia from "./sections/Lucia";
+import PostCard from "./sections/Postcard";
+import Final from "./sections/FinalVideo";
+import Outro from "./sections/Outro";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <div className="text-hi">Hello World</div>
+      <Navbar />
+      <Hero />
+      <FirstVideo />
+      <Jason />
+      <SecondVideo />
+      <Lucia />
+      <PostCard />
+      <Final />
+      <Outro />
     </main>
   );
 };
